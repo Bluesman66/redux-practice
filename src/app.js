46b5af907a86b7370860ea06afbc425d05b0ba2b
alex.paclin <alex.paclin@gmail.com>
2020-01-19 02:17:27 +0500

    usersState

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const reducer = (state = {}, action) => {
+const usersState = ['@john_123', '@alex_1966', '@chris'];
+const reducer = (state = usersState, action) => {
     return state;
 };
 const store = createStore(reducer);
